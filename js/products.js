f73a1d22be4de6f0dd3b69a62c63465f19b178d2
o-strich <kalilosta1@gmail.com>
2025-08-23 18:47:44 -0300

    añadido el catalogo de productos

---
+++ js/products.js
@@ -0,0 +1,35 @@
+
+let urlAutos="https://japceibal.github.io/emercado-api/cats_products/101.json"
+const listaProductos= document.getElementById("catalogoAutos")
+async function fetchProductos(url){
+    try{
+        response= await(fetch(url))
+        if (!response.ok)
+            throw Error(response.statusText);
+        const data= await(response.json())
+
+
+        for(producto of data.products){
+
+            const info=[
+                `Nombre:${producto.name}`,
+                `Descripcion:${producto.description}`,
+                `Precio:${producto.cost}`,
+                `Cantidad de productos vendidos:${producto.soldCount}`
+            ]
+                let elemento= document.createElement("li")
+            info.forEach(atributo=>{
+                const p=document.createElement('p')
+                p.textContent=atributo
+                elemento.appendChild(p)
+            })
+            const image= document.createElement('img')
+            image.src=producto.image
+            elemento.appendChild(image)
+            listaProductos.appendChild(elemento)
+         }
+    }catch(error){
+        console.log(`Error found: ${error}`)
+    }
+}
+fetchProductos(urlAutos)
