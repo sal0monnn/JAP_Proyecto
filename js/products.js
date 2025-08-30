ca3d58dd6b3e10b34efc5cc67b4e23c8004214bf
sal0monnn <salomon15gon@gmail.com>
2025-08-30 19:03:56 -0300

    css de productos
    agregado para que quede lo más similar al prototipo

--- js/products.js
+++ js/products.js
@@ -9,14 +9,14 @@ function showProductsList(productsArray) {
 
         htmlContentToAppend += `
             <div class="col-md-6 col-sm-12 col-lg-4 mb-4">
-                <div class="card h-100 shadow-sm border-0 rounded-3 custom-card cursor-active" onclick="setProductID(${product.id})">
+                <div class="card h-100 shadow-sm border-0 rounded-4 custom-card cursor-active elementList" onclick="setProductID(${product.id})">
                     <img src="${product.image}" class="card-img-top img-fluid" alt="${product.name}">
                     <div class="card-body p-3">
                         <h5 class="card-title">${product.name}</h5>
-                        <p class="card-text text-muted">${product.description}</p>
+                        <p class="card-text">${product.description}</p>
                         <div class="d-flex justify-content-between align-items-center">
                             <strong>${priceToShow}</strong>
-                            <small class="text-muted">${soldCountText}</small>
+                            <small class="textSold">${soldCountText}</small>
                         </div>
                     </div>
                 </div>
