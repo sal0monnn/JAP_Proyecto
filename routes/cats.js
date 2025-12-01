6b95b23a473e852c9c54a0cfce83178a0b36ee5e
Ian <eduiancastro@gmail.com>
2025-11-30 21:42:18 -0300

    Update cats.js

--- routes/cats.js
+++ routes/cats.js
@@ -1,10 +1,10 @@
-const catsRoutes = (app, fs) => {
-  const ruta = './api/cats/cat.json';  
-  app.get('/cats', (req, res) => {
+const catsRoutes = (app, fs, authMiddleware) => {
+  const ruta = './api/cats/cat.json';
+
+  app.get('/cats', authMiddleware, (req, res) => {
     fs.readFile(ruta, 'utf8', (err, data) => {
       if (err) {
-        res.status(500).send('Error al leer el archivo');
-        return;
+        return res.status(500).send('Error al leer el archivo');
       }
       res.send(JSON.parse(data));
     });
