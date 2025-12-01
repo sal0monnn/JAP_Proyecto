f64c2769d3f2b7114aa7f0bfc8336ae4b9260f35
Ian <eduiancastro@gmail.com>
2025-11-30 21:40:35 -0300

    Add files via upload

---
+++ routes/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  // Verificar si llega el header Authorization
+  if (!authHeader) {
+    return res.status(401).json({ mensaje: "Token no proporcionado" });
+  }
+
+  // El token viene como: "Bearer xxxx..."
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, "CLAVE_SECRETA_JWT");
+    req.user = decoded; // guarda info del usuario
+    next(); // permite seguir
+  } catch (error) {
+    return res.status(403).json({ mensaje: "Token inválido o expirado" });
+  }
+}
+
+module.exports = authMiddleware;
