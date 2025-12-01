c3374ce231e11c7c861f7317777587c17390b9ac
Ian <eduiancastro@gmail.com>
2025-11-30 21:42:38 -0300

    Update comment_routes.js

--- routes/comment_routes.js
+++ routes/comment_routes.js
@@ -1,16 +1,16 @@
-const commentRoutes = (app, fs) => {
-  app.get('/products/:id/comments', (req, res) => {
-    const rutaComentario = `./api/products_comments/${req.params.id}.json`;  
+module.exports = (app, fs, authMiddleware) => {
+
+  app.get('/products/:id/comments', authMiddleware, (req, res) => {
+    const rutaComentario = `./api/products_comments/${req.params.id}.json`;
+
     fs.readFile(rutaComentario, 'utf8', (err, data) => {
       if (err) {
-        res.status(500).send('Error al leer el archivo');
-        return;
+        return res.status(500).send('Error al leer el archivo');
       }
-      res.send(JSON.parse(data));
+      res.json(JSON.parse(data));
     });
   });
-};
 
-module.exports = commentRoutes;
+};
 
 
