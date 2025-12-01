3a8f24669aa8e20b15ccfea09859a595ee15caed
Ian <eduiancastro@gmail.com>
2025-11-30 21:39:45 -0300

    Middleware

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
-const express= require('express')
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
 
-const app = express()
-const fs = require('fs');
-const cors = require('cors');   
+const authMiddleware = require("./authMiddleware");
+
+// Importar rutas
+const catsRoutes = require("./cats.js");
+const productsRoutes = require("./cat_producto.js");
+const productInfoRoutes = require("./product_info.js");
+const commentRoutes = require("./comment_routes.js");
+const publishRoute = require("./publish.js");
+const buyRoute = require("./buy_route.js");
+const cartInfoRoute = require("./cart_info_route.js");
+
+const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-const routes = require('./routes/routes.js')(app, fs);
+// LOGIN (sin middleware)
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
 
-const port=3000;
+  if (!username || !password) {
+    return res.status(400).json({ mensaje: "Faltan datos" });
+  }
 
-const server = app.listen(port, () => {
-  console.log('activo en puerto ...', server.address().port);
+  const token = jwt.sign(
+    { username },
+    "CLAVE_SECRETA_JWT",
+    { expiresIn: "2h" }
+  );
+
+  res.json({
+    mensaje: "Login exitoso",
+    token,
+    usuario: username
+  });
+});
+
+// Todas las rutas protegidas
+catsRoutes(app, fs, authMiddleware);
+productsRoutes(app, fs, authMiddleware);
+productInfoRoutes(app, fs, authMiddleware);
+commentRoutes(app, fs, authMiddleware);
+publishRoute(app, fs, authMiddleware);
+buyRoute(app, fs, authMiddleware);
+cartInfoRoute(app, fs, authMiddleware);
+
+// Iniciar servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Servidor funcionando en http://localhost:" + PORT);
 });
